d3c4a4295228aa0c2c2b866e8725d35946d0923a
futureliveparticeroom <145907111+futureliveparticeroom@users.noreply.github.com>
2023-09-28 03:06:35 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const path = require('path');
 const app = express();
 const port = 3002;
 
+app.use(express.static('public')); // 假設您有一個名為 'public' 的目錄包含靜態檔案，例如 'success.html'
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './public/uploads/');
+    cb(null, path.join(__dirname,'public', 'uploads'));
   },
   filename: (req, file, cb) => {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
@@ -17,34 +19,26 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-app.use(express.static('public'));
-
 app.get('/', (req, res) => {
-  res.sendFile('/public/uploads');
-
-
-
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
-
-
-
 app.post('/upload', upload.single('video'), (req, res) => {
-  res.redirect('success.html');
-
   if (!req.file) {
     return res.status(400).send('No file uploaded.');
   }
 
   const uploadedVideoPath = req.file.path;
-  const gifPath = uploadedVideoPath.split('.').slice(0, -1).join('.') + '.webm';
+  const gifPath = path.join(__dirname,'public', 'videos', Date.now() + '.webm');
 
   ffmpeg(uploadedVideoPath)
     .toFormat('webm')
+    .on('end', () => {
+      res.redirect('success.html');
+    })
     .on('error', (err) => {
       console.error(err);
-      res.redirect('error.html');
+      res.status(500).redirect('error.html'); // 注意，如果 'end' 事件已經發生，這將引發錯誤
     })
     .save(gifPath);
 });
